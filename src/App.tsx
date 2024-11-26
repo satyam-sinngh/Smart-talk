d18447ba40cf5c350e9b3d512a5b4f084585605f
Satyam Kumar <satyamsinghbgs90@gmail.com>
2024-11-26 22:55:37 +0530

    creates UI fro homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,72 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import "./App.css"
+import {IoCodeSlash, IoSend} from "react-icons/io5";
+import {BiPlanet} from "react-icons/bi";
+import {FaPython} from "react-icons/fa";
+import {TbMessageChatbot} from "react-icons/tb";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+const App: React.FC = () => {
+    return (
+        <>
+            <main className={"container  min-w-full min-h-screen overflow-x-hidden bg-[#0E0E0E] text-white"}>
+                <section className={"middle min-h-[70vh] flex items-center justify-center flex-col"}>
+                    <h2 className={"text-4xl"}>Smart Talk </h2>
+                    <div className={"boxes mt-[30px] flex items-center gap-2"}>
+                        <div
+                            className={"card rounded-lg px-[20px] relative min-h-[20vh] bg-[#181818] p-[10px] cursor-pointer transition-all hover:bg-[#201f1f] duration-300"}>
+                            <p className={"text-[18px]"}>
+                                what is coding?<br/>
+                                how can we learn it.
+                            </p>
+                            <i className={"absolute right-3 bottom-3 text-[18px]"}><IoCodeSlash/></i>
+                        </div>
+                        <div
+                            className={"card rounded-lg px-[20px] relative min-h-[20vh] bg-[#181818] p-[10px] cursor-pointer transition-all hover:bg-[#201f1f] duration-300"}>
+                            <p className={"text-[18px]"}>
+                                what is the red?<br/>
+                                planet of solar<br/>
+                                system?
+                            </p>
+                            <i className={"absolute right-3 bottom-3 text-[18px]"}><BiPlanet/></i>
+                        </div>
+                        <div
+                            className={"card rounded-lg px-[20px] relative min-h-[20vh] bg-[#181818] p-[10px] cursor-pointer transition-all hover:bg-[#201f1f] duration-300"}>
+                            <p className={"text-[18px]"}>
+                                In which year python<br/>
+                                was invented?
+                            </p>
+                            <i className={"absolute right-3 bottom-3 text-[18px]"}><FaPython/></i>
+                        </div>
+                        <div
+                            className={"card rounded-lg px-[20px] relative min-h-[20vh] bg-[#181818] p-[10px] cursor-pointer transition-all hover:bg-[#201f1f] duration-300"}>
+                            <p className={"text-[18px]"}>
+                                How can we can use<br/>
+                                the AI for adopt?
+                            </p>
+                            <i className={"absolute right-3 bottom-3 text-[18px]"}><TbMessageChatbot/></i>
+                        </div>
+                    </div>
+                </section>
+                <section className={"bottom w-full flex flex-col justify-center items-center"}>
+                    <div
+                        className={"inputBox flex items-center bg-[#181818] rounded-[30px] w-[60%] text-[15px] py-[7px]"}>
+                        <input
+                            type={"text"}
+                            placeholder={"Write your message here..."}
+                            id={"messageBox"}
+                            className={"p-[10px] bg-transparent flex-1 outline-0 border-none text-white pl-[15px]"}
+                        />
+                        <i className={"text-green-500 text-[20px] mr-5 cursor-pointer"}>
+                            <IoSend/>
+                        </i>
+                    </div>
+                    <p className={"text-[gray] text-[14px] my-4"}>AssistMe is developed by Mo. Mahdi Farooqui. this AI
+                        use the gemini API for giving
+                        the
+                        response </p>
+                </section>
+            </main>
+        </>
+    )
 }
-
 export default App
